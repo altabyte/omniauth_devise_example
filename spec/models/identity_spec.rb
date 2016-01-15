c384c0d02a5d76a6d6fa0ee9cde5456723d35c74
Rob Graham <altabyte@gmail.com>
2016-01-15 03:04:26 +0000

    Configure Shoulda matchers.

--- spec/models/identity_spec.rb
+++ spec/models/identity_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Identity, type: :model do
 
+  it { is_expected.to belong_to :user }
+
   it { expect(FactoryGirl.create(:identity)).to be_valid }
   it { expect(FactoryGirl.build(:identity, provider: nil)).not_to be_valid }
   it { expect(FactoryGirl.build(:identity, uid: nil)).not_to be_valid }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  subject(:user) { FactoryGirl.create(:user) }
-
-  it { is_expected.to be_valid }
-
+  it { is_expected.to have_many(:identities) }
+  it { expect(FactoryGirl.create(:user)).to be_valid }
 end
