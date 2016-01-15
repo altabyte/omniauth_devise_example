22e76e1653a9e95645a89c692114b01b1286c827
Rob Graham <altabyte@gmail.com>
2016-01-15 18:18:43 +0000

    Add find_for_oauth method to User model.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,6 @@ FactoryGirl.define do
     sequence(:email)    { |n| "user_#{n}@email.com" }
     password '12345678'
     password_confirmation '12345678'
+    confirmed_at  { Time.now }
   end
 end
